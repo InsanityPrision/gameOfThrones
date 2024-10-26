2098d6b559e928e8aba7fb30b8addd6881456501
Erik Riquelme <erikone2311@gmail.com>
2024-10-26 15:46:28 +0200

    Create a test for getCharactersPhrases

---
+++ src/characters/__tests__/getCharactersPhrases.test.ts
@@ -0,0 +1,20 @@
+import getCharactersPhrases from "..";
+import { daenerys, jaime } from "../fighter/data/data";
+import bronn from "../squire/data/data";
+
+describe("Given the getCharactersPhrases", () => {
+  describe("When it receives a list of 3 characters", () => {
+    test("Then it should return a list of thre phrases", () => {
+      const expectedPhrases = [
+        "Primero pego y luego pregunto",
+        "Soy un loser",
+        "Primero pego y luego pregunto",
+      ];
+
+      const phrases = getCharactersPhrases([jaime, bronn, daenerys]);
+      console.log(phrases);
+
+      expect(phrases).toEqual(expectedPhrases);
+    });
+  });
+});
