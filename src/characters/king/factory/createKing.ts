e6e908699eea4e1273bf8974cf5d3b6b8ed43933
Erik Riquelme <erikone2311@gmail.com>
2024-10-26 12:12:11 +0200

    Create a king factory

---
+++ src/characters/king/factory/createKing.ts
@@ -0,0 +1,18 @@
+import { createCharacter } from "../../factory/createCharacter";
+import { type CharacterData } from "../../types";
+import { type King } from "../types";
+
+const createKing = (
+  characterData: CharacterData,
+  phrase: string,
+  yearsOfReign: number,
+) => {
+  const king: King = {
+    ...createCharacter(characterData, phrase),
+    yearsOfReign,
+  };
+
+  return king;
+};
+
+export default createKing;
--- src/characters/king/types.ts
+++ src/characters/king/types.ts
@@ -2,6 +2,4 @@ import { type Character } from "../types";
 
 export type King = Character & {
   yearsOfReign: number;
-  phrase: "Vais a morir todos";
-  talk: () => string;
 };
