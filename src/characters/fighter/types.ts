a883281b6897381c8d2fd98b3162ca7738f77a65
Erik Riquelme <erikone2311@gmail.com>
2024-10-26 10:54:21 +0200

    Create a factory for characters

--- src/characters/fighter/types.ts
+++ src/characters/fighter/types.ts
@@ -5,5 +5,4 @@ export type SkillLevel = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 export type Fighter = Character & {
   weapon: string;
   skillLevel: SkillLevel;
-  phrase: "Primero pego y luego pregunto";
 };
