4871cb2e2366eb29136b04c6dff0716e54b46020
Erik Riquelme <erikone2311@gmail.com>
2024-10-30 23:24:12 +0100

    Create app component

--- src/components/app/index.ts
+++ src/components/app/index.ts
@@ -1,6 +1,6 @@
-import createCardsContainer from "./characters/components/cards-container/index.js";
-import { characters } from "./characters/data/data.js";
-import createMainHeader from "./components/header/index.js";
+import createCardsContainer from "../../characters/components/cards-container/index.js";
+import { characters } from "../../characters/data/data.js";
+import createMainHeader from "../header/index.js";
 
 const mainContainer = document.querySelector(".root");
 
