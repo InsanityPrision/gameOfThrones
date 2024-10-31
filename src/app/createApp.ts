d1cf24e27dc21e63d1e0fbc793713f83de03d353
Erik Riquelme <erikone2311@gmail.com>
2024-10-31 16:38:02 +0100

    Create app component

---
+++ src/app/createApp.ts
@@ -0,0 +1,15 @@
+import createCardsContainer from "../characters/components/cardsContainer/index.js";
+import { type Character } from "../characters/types";
+import createMainHeader from "../components/header/index.js";
+
+const createApp = (characters: Character[]): HTMLElement => {
+  const mainContainer = document.createElement("div");
+  mainContainer.classList.add("main-container");
+
+  mainContainer.appendChild(createMainHeader());
+  mainContainer.appendChild(createCardsContainer(characters));
+
+  return mainContainer;
+};
+
+export default createApp;
