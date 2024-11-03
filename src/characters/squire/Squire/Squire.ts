0bc513fda86c6aca5211fae262ba28f0e3c031b0
Erik Riquelme <erikone2311@gmail.com>
2024-11-03 22:23:08 +0100

    Add character information to hover card

--- src/characters/squire/Squire/Squire.ts
+++ src/characters/squire/Squire/Squire.ts
@@ -1,11 +1,11 @@
 import { Character } from "../../Character/Character.js";
-import { type FighterStructure } from "../../fighter/types";
+import type Fighter from "../../fighter/Fighter/Fighter.js";
 import { type CharacterData } from "../../types";
 import { type ToadyLevel, type SquireStrucutre } from "../types";
 export class Squire extends Character implements SquireStrucutre {
   constructor(
     characterData: CharacterData,
-    public boss: FighterStructure,
+    public boss: Fighter,
     public toadyLevel: ToadyLevel,
   ) {
     super(characterData);
